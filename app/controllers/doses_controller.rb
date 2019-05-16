dd7da091b58bd3f6e3655935c0711b8fa75c549d
guerrique desmoulins guerrique@gmail.com <guerrique@gmail.com>
2019-05-16 16:20:24 +0200

    implemented the delete dose truc

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,4 +1,5 @@
 class DosesController < ApplicationController
+before_action :set_dose, only: [:destroy]
 
   def new
     @cocktail = Cocktail.find(params[:cocktail_id])
@@ -17,7 +18,16 @@ class DosesController < ApplicationController
     end
   end
 
+  def destroy
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail.id)
+  end
+
   private
+
+  def set_dose
+    @dose = Dose.find(params[:id])
+  end
 
   def dose_params
     params.require(:dose).permit(:description, :ingredient_id)
